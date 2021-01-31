b5a768aaf12f2dfd4f48540cb798738d50192421
Yetaeng <yetaeng20@gmail.com>
2021-01-31 20:48:08 +0900

    실습4. 솔루션 후 수정

--- chapter3/coordinates/main.js
+++ chapter3/coordinates/main.js
@@ -1,10 +1,20 @@
-const toolTip = document.querySelector('.toolTip')
+const horizontal = document.querySelector('.horizontal')
+const vertical = document.querySelector('.vertical')
+const target = document.querySelector('.target')
+const tooltip = document.querySelector('.tooltip')
 
 window.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
 
-    toolTip.innerText = `${event.pageX}px, ${event.pageY}px`;
-    toolTip.style.top = (y + 70) + 'px';
-    toolTip.style.left = (x + 70) + 'px';
+    horizontal.style.top = `${y}px`
+    vertical.style.left = `${x}px`
+
+    target.style.top = `${y}px`
+    target.style.left = `${x}px`
+
+    tooltip.style.top = `${y}px`
+    tooltip.style.left = `${x}px`
+
+    tooltip.innerText = `${event.pageX}px, ${event.pageY}px`;
 }) 
