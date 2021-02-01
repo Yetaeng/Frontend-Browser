deb983b8b1eadbfc8ebf28c6936c88c0c575fcba
Yetaeng <yetaeng20@gmail.com>
2021-02-01 23:12:05 +0900

    실습 6. 좌표 실습 성능 개선

--- chapter3/coordinates/main.js
+++ chapter3/coordinates/main.js
@@ -7,14 +7,20 @@ window.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
 
-    horizontal.style.top = `${y}px`
-    vertical.style.left = `${x}px`
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2
+    const targetHalfHeight = targetRect.height / 2
 
-    target.style.top = `${y}px`
-    target.style.left = `${x}px`
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
 
-    tooltip.style.top = `${y}px`
-    tooltip.style.left = `${x}px`
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
 
+
+    tooltip.style.transform = `translate(${x}px, ${y}px)`;
     tooltip.innerText = `${event.pageX}px, ${event.pageY}px`;
 }) 
+
+// top이나 left는 layout부터 발생하는 속성이므로 애니메이션을 사용할 때 좋지 않다.
+// 대신 composite만 일어나는 translate를 쓰면 성능 개선에 굿
+// layout -> paint -> composite
