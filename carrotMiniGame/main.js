92a89f769215554bca8c5717bf3cc5537c629352
Yetaeng <yetaeng20@gmail.com>
2021-03-28 23:58:42 +0900

    게임 시작, 중지, 다시 시작 구현 및 타이머 동작 수정

--- carrotMiniGame/main.js
+++ carrotMiniGame/main.js
@@ -3,20 +3,116 @@
 const area = document.querySelector('.game_area');
 const areaRect = area.getBoundingClientRect();
 const carrotSize = 80;
+const carrotCount = 5;
+const bugCount = 5;
 const gameBtn = document.querySelector('.game_btn');
+const gameTimer = document.querySelector('.game_timer');
+const gameCnt = document.querySelector('.game_cnt');
+const setTime = 5;
+const replayBtn = document.querySelector('.replay_btn');
+
+// 게임의 상태를 기억할 수 있는 변수 필요
+let started = false; // 게임이 시작되지 않은 상태
+
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    
+    started = !started // 현 started의 상태와 반대로 만듬
+})
+
+replayBtn.addEventListener('click', () => {
+    startGame();
+    hidePopup();
+    started = !started;
+})
+
+function startGame() { 
+    initGame();
+    showStopBtn();
+    showTimerAndCount();
+    startTimer();
+    hidePopup();
+}
+
+function stopGame() {
+    showPlayBtn();
+    stopTimer();
+    showPopup();
+}
+
+function showStopBtn() {
+    const icon =  gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showPlayBtn() {
+    const icon =  gameBtn.querySelector('.fa-stop');
+    icon.classList.add('fa-play');
+    icon.classList.remove('fa-stop');
+}
+
+function showTimerAndCount() {
+    gameTimer.style.visibility = 'visible';
+    gameCnt.style.visibility = 'visible';
+}
+
+///////////////////////////////
+let nIntervId;
+let time = setTime;
+
+function startTimer() {
+    nIntervId = setInterval(countDown, 1000);
+    }
+
+function countDown() {
+    let min = '';
+    let sec = '';
+
+    time--;
+    min = parseInt(time / 60); // parseInt는 정수로 반환하는 함수이며, 이를 이용해 몫을 계산
+    sec = time % 60; // 나머지를 계산
+    gameTimer.innerHTML = `${min}:${sec}`;
+
+    if (time == 0) {
+        stopTimer();
+        showPopup();
+    }
+}
+
+function stopTimer() {
+    clearInterval(nIntervId);
+    return time = setTime;
+}
+///////////////////////////////
+
+function showPopup() {
+    const popup = document.querySelector('.popup');
+    popup.classList.remove('popup_hide');
+}
+
+function hidePopup() {
+    const popup = document.querySelector('.popup');
+    popup.classList.add('popup_hide');
+}
 
 function initGame() {
-    console.log(areaRect);
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png');
-    countItem(5);
+    area.innerHTML = '' // 처음에 remove()를 써서 아이템을 하나하나 지웠는데, 그냥 innerHtml = ''로 초기화하여 지울 수 있다.
+
+    addItem('carrot', carrotCount, 'img/carrot.png');
+    addItem('bug', bugCount, 'img/bug.png');
+    gameCnt.innerText = carrotCount;
+    gameTimer.innerHTML = `0:${setTime}`;
 }
 
 function addItem(itemName, count, imgPath) {
-    // x1, x2, y1, y2를 이용해 특정 범위 내에 속하는 난수를 구한다.
     const x1 = 0;
     const y1 = 0;
-    const x2 = areaRect.width - carrotSize; // bug는 50인데 가장 큰 거를 빼주자.
+    const x2 = areaRect.width - carrotSize;
     const y2 = areaRect.height - carrotSize;
 
     for (let i=0; i<count; i++) {
@@ -37,43 +133,3 @@ function addItem(itemName, count, imgPath) {
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-function removeAllItem() {
-    const carrot = document.querySelectorAll('.carrot');
-    const bug = document.querySelectorAll('.bug');
-
-    for (let i=0; i<carrot.length; i++) {
-        carrot[i].remove()
-        bug[i].remove()
-    }
-}
-
-function timer(setTimeSecond) {
-    let gameTimer = document.querySelector('.game_timer');
-    let setT;
-
-    gameTimer.innerText = `0:${setTimeSecond}`;
-    for(let i=0; i<=setTimeSecond; i++) {
-        setT = setTimeout(() => { gameTimer.innerText = `0:${setTimeSecond-i}` }, `${i}000`);
-    }
-    // clearTimeout(setT)
-}
-
-function countItem(count) {
-    let gameCnt = document.querySelector('.game_cnt');
-    gameCnt.innerText = `${count}`
-}
-
-gameBtn.addEventListener('click', () => {
-    console.log('Game Start!')
-    removeAllItem();
-    initGame();
-    timer(5);
-})
-
-
-// game_area를 position:relative를 주고, item을 position으로 주면 그 영역 내에서 x, y대로 배치시킬 수 있다.
-
-// 나는 난수를 구할 떄 0 이상 1 미만의 난수 생성하기를 했는데, 엘리쌤은 두 값 사이의 난수 생성하기를 하심
-// 그래서 나는 소수점에 1000 같은 수를 곱해서 코드를 짰고,
-// 엘리쌤처럼 하면 두 값(맨 왼쪽부터 맨 오른쪽이랑 맨 위부터 맨 아래)사이의 난수를 그냥 구할 수 있다.
