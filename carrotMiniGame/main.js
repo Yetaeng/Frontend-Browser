27f5ad7f0ce79cdafa826cb6df037bddaa25cfec
Yetaeng <yetaeng20@gmail.com>
2021-04-04 23:37:56 +0900

    제한 시간 내에 당근 다 먹으면 승, 못먹거나 벌레 누르면 게임 오버 구현

--- carrotMiniGame/main.js
+++ carrotMiniGame/main.js
@@ -10,32 +10,40 @@ const gameTimer = document.querySelector('.game_timer');
 const gameCnt = document.querySelector('.game_cnt');
 const setTime = 5;
 const replayBtn = document.querySelector('.replay_btn');
+const popupMsg = document.querySelector('.popup_msg');
 
-// 게임의 상태를 기억할 수 있는 변수 필요
-let started = false; // 게임이 시작되지 않은 상태
+let started = false;
 
+// 해결해야 할 것 : 게임을 지거나 이긴 후, 게임 중지버튼을 누르면 오류
+
+/* 이벤트 */
 gameBtn.addEventListener('click', () => {
     if(started) {
         stopGame();
+        changePopupMsg('replay');
     } else {
         startGame();
     }
-    
-    started = !started // 현 started의 상태와 반대로 만듬
+    started = !started
 })
 
 replayBtn.addEventListener('click', () => {
+    started = !started;
     startGame();
     hidePopup();
-    started = !started;
+
 })
 
+
+/* 함수 */
 function startGame() { 
     initGame();
     showStopBtn();
     showTimerAndCount();
     startTimer();
     hidePopup();
+    removeCarrot();
+    checkClickBug();
 }
 
 function stopGame() {
@@ -44,6 +52,46 @@ function stopGame() {
     showPopup();
 }
 
+/* step 3 */
+function removeCarrot() {
+    const carrot = document.querySelectorAll('.carrot');
+    for (let i=0; i<carrot.length; i++) {
+        carrot[i].addEventListener('click', () => {
+            gameCnt.textContent--;
+            carrot[i].remove()
+            
+            if (gameTimer.textContent != '0:0' && gameCnt.textContent == '0') {
+                stopGame();
+                changePopupMsg('win');
+            }
+        })
+    }
+}
+
+function changePopupMsg(param) {
+    if (param == 'win') {
+        popupMsg.innerText = "You Win!"
+    }
+    else if (param == 'lose') {
+        popupMsg.innerText = "You Lose!"
+    }
+    else if (param == 'replay') {
+        popupMsg.innerText = "Replay?"
+    }
+}
+
+function checkClickBug() {
+    const bug = document.querySelectorAll('.bug');
+
+    for (let i=0; i<bug.length; i++) {
+        bug[i].addEventListener('click', () => {
+            stopGame();
+            changePopupMsg('lose');
+        })
+    }
+}
+
+/* step 2 */
 function showStopBtn() {
     const icon =  gameBtn.querySelector('.fa-play');
     icon.classList.add('fa-stop');
@@ -61,7 +109,6 @@ function showTimerAndCount() {
     gameCnt.style.visibility = 'visible';
 }
 
-///////////////////////////////
 let nIntervId;
 let time = setTime;
 
@@ -74,13 +121,13 @@ function countDown() {
     let sec = '';
 
     time--;
-    min = parseInt(time / 60); // parseInt는 정수로 반환하는 함수이며, 이를 이용해 몫을 계산
-    sec = time % 60; // 나머지를 계산
+    min = parseInt(time / 60);
+    sec = time % 60;
     gameTimer.innerHTML = `${min}:${sec}`;
 
     if (time == 0) {
-        stopTimer();
-        showPopup();
+        stopGame();
+        changePopupMsg('lose');
     }
 }
 
@@ -88,20 +135,26 @@ function stopTimer() {
     clearInterval(nIntervId);
     return time = setTime;
 }
-///////////////////////////////
 
 function showPopup() {
     const popup = document.querySelector('.popup');
     popup.classList.remove('popup_hide');
+
+    // 팝업창이 있다면, gameBtn 비활성화하기 (To do - 스케일 없애기)
+    if(showPopup) {
+        gameBtn.disabled = true;
+    }
 }
 
 function hidePopup() {
     const popup = document.querySelector('.popup');
     popup.classList.add('popup_hide');
+    gameBtn.disabled = false;
 }
 
+/* step 1 */
 function initGame() {
-    area.innerHTML = '' // 처음에 remove()를 써서 아이템을 하나하나 지웠는데, 그냥 innerHtml = ''로 초기화하여 지울 수 있다.
+    area.innerHTML = '';
 
     addItem('carrot', carrotCount, 'img/carrot.png');
     addItem('bug', bugCount, 'img/bug.png');
